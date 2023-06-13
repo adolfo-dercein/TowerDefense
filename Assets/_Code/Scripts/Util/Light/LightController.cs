43960c62f69e1fcd11573f09fdd6af824e73506a
Ana López <anamaria.lopez@endava.com>
2023-06-13 10:35:59 -0300

    save progress

---
+++ Assets/_Code/Scripts/Util/Light/LightController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class LightController : MonoBehaviour
+{
+    public float minIntensity = 0.02f;
+    public float maxIntensity = 0.03f;
+    public float Timer = 0.1f;
+    private new Light light = null;
+
+    private void Start()
+    {
+        light = this.GetComponentInChildren<Light>();
+        InvokeRepeating("Flicker", Timer, Timer);
+    }
+    private void Flicker()
+    {
+        float R = UnityEngine.Random.Range(minIntensity, maxIntensity);
+        light.intensity = R;
+    }
+}
