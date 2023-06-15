bdadaa3304bf1b0ffcd9de0037c107b7653139f7
Dercein <merino.adolfo@gmail.com>
2023-06-15 16:39:30 -0300

    Info Panel (again)

--- Assets/_Code/Scripts/Enemy/States/EnemyWinState.cs
+++ Assets/_Code/Scripts/Enemy/States/EnemyWinState.cs
@@ -4,8 +4,6 @@ namespace Assets._Code.Scripts.Enemy.States
 {
     public class EnemyWinState : EnemyBaseState
     {
-        GameManagerController gameManagerController;
-
         public override void EnterState(EnemyController enemyController)
         {
             enemyController.gameManagerController.AddEnemyPassCount();
